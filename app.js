216e0d4050550f92d0aee40703f880ab2483f924
Shubh Singh <shubhsinghrajpoot6665@gmail.com>
2025-04-28 14:30:09 +0530

    Fixed error handling and added ExpressError class and wrapAync

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const Listing = require("./models/listing")
 const path = require("path");
 const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/Expresserror"); 
 
- 
 main()
 .then(()=>{
     console.log("Connected to DB");
@@ -31,12 +32,13 @@ app.get("/",(req,res)=>{
 })
 
  //Index Route(Step 5)
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
     const allListings=await Listing.find({}); //yha pr hmne sare listings ko ek var me store kiya hai aur phir render
     res.render("./listings/index.ejs",{allListings});
-})
+}));
 
 //New Route(Step 7 i)
+
 // jb bhi new listing bnani hoti hai toh do kaam krna hota hai 
 // get request(New) aur post request(Create)
 // get request se new.ejs file ko render krna hota hai
@@ -46,43 +48,49 @@ app.get("/listings/new",(req,res)=>{
 })
 
 //Show Route(Step 6)
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listingid=await Listing.findById(id);
     res.render("./listings/show.ejs",{listingid});
-})
+}));
 
 //Crete Route(Step 7 ii)
-app.post("/listings",async(req,res)=>{
+app.post("/listings",wrapAsync(async(req,res,next)=>{
     // 1st way -> let {title,description,image,price,location,country}=req.body;
     // 2nd way -> let listing=req.body.listing;  new Listing(listing);
     // optimized of 2nd way
+    if(!req.body.listing){
+        throw new ExpressError(400,"Send Valid Lisiting Data");
+    } 
     const newListing=new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-})
+}));
 
 //Edit Route (Step 8 i)
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     let{id}=req.params;
     const listingid = await Listing.findById(id);
     res.render("./listings/edit.ejs" ,{listingid});
-})
+}));
 
 
 //Update Route (Step 8 ii)
-app.put("/listings/:id", async(req,res)=>{
+app.put("/listings/:id", wrapAsync(async(req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"Send Valid Lisiting Data");
+    } 
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-})
+}));
 
-app.delete("/listings/:id" ,async(req,res)=>{
+app.delete("/listings/:id" ,wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const deletedList=await Listing.findByIdAndDelete(id);
     console.log(deletedList);
     res.redirect("/listings");
-})
+}));
 
 // TestingSample Listing
 
@@ -101,6 +109,15 @@ app.delete("/listings/:id" ,async(req,res)=>{
 //     res.send("Testing Successful");
 // })
 
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404, "Page Not Found!")); 
+})
+
+app.use((err,req,res,next)=>{
+    let {statusCode=500 , message="Something Went Wrong!"}=err;
+    res.status(statusCode).send(message );
+     
+})
 
 app.listen(8080,()=>{
     console.log("server is listening to port 8080");
