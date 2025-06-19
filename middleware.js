e7b27211ef0e121e362a30efeb83b369273c40df
Shubh Singh <shubhsinghrajpoot6665@gmail.com>
2025-06-19 21:47:04 +0530

    Refactor user authentication routes to include redirect URL handling and improve user registration flow

--- middleware.js
+++ middleware.js
@@ -1,7 +1,19 @@
 module.exports.isLoggedIn=(req,res,next)=>{
     if(!req.isAuthenticated()){
+        //Before login where the user was trying to go 
+        req.session.redirectUrl=req.originalUrl;
         req.flash("error","You Must Be Logged In!");
         return res.redirect("/login");
     }
     next();
 };
+
+
+//req.session gets deleted by passport.authenticate when someone logs in so store in locals
+//so run this before passport.authenticate
+module.exports.saveredirectUrl=(req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl=req.session.redirectUrl;
+    }
+    next();
+}
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/user.js');
 const wrapAsync = require('../utils/wrapAsync.js');
 const passport = require('passport');
+const { saveredirectUrl } = require('../middleware.js');
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
@@ -15,31 +16,38 @@ router.post("/signup", wrapAsync(async (req, res) => {
             email,
             username
         })
-        await User.register(newUser, password);
+        const registeredUser = await User.register(newUser, password);
+        req.login(registeredUser, (err) => {    //signup krte hii loggedIn
+            if (err) {
+                return next(err);
+            } else {
+                req.flash("success", "User Registered Successfully");
+                res.redirect("/listings");
+            }
+        })
 
-        req.flash("success", "User Registered Successfully");
-        res.redirect("/listings");
-    }catch(e){
-        req.flash("error",e.message);
+    } catch (e) {
+        req.flash("error", e.message);
         res.redirect("/signup");
     }
 }));
 
-router.get("/login", (req,res)=>{
+router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 })
 
-router.post("/login",passport.authenticate("local",{failureRedirect:'/login',failureFlash:true}), async(req,res)=>{
-    req.flash("success","Login Successful!"); 
-    res.redirect("/listings");
+router.post("/login", saveredirectUrl, passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }), async (req, res) => {
+    req.flash("success", "Login Successful!");
+    let redirectUrl=req.session.redirectUrl || '/listings';
+    res.redirect(redirectUrl);
 })
 
-router.get("/logout", (req,res,next)=>{
-    req.logout((err)=>{
-        if(err){
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
             return next(err);
-        }else{
-            req.flash("success","Logged Out Successfully!");
+        } else {
+            req.flash("success", "Logged Out Successfully!");
             res.redirect("/listings");
         }
     })
